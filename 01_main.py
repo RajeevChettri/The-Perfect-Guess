18a5aaa6fcabd8a85976b88791a403fd17550206
Rajeev Chettri <rcnytfamilygaming@gmail.com>
2025-07-12 21:34:39 +0530

    Added the code in 2nd commit

---
+++ 01_main.py
@@ -0,0 +1,18 @@
+# To guess number from 1 to 100
+import random as r
+n = r.randint(1,100)
+
+guesses = 0
+a = -1
+while(a != n):
+    a = int(input("Guess the number: "))
+    
+    if(n<a):
+        print("Lower number please")
+        guesses += 1
+        
+    else:
+        print("Higher number please")
+        guesses +=1
+
+print(f"You have guessed the correct number ({n}) in {guesses} attempts.")
