aa5e5b6970aa0612256238074de02e94e711a1da
Rajeev Chettri <rcnytfamilygaming@gmail.com>
2025-07-12 22:18:52 +0530

    Same as main only change is guesses is moved

---
+++ 02_same_main.py
@@ -0,0 +1,19 @@
+# To guess number from 1 to 100
+import random as r
+n = r.randint(1,100)
+print(n)
+print("Guess the number from 1 to 100!")
+guesses = 0
+a = -1
+while(a != n):
+    # Only guess is moved to the top!!
+    guesses += 1
+    a = int(input("Guess the number: "))
+    
+    if(n<a):
+        print("Lower number please")
+        
+    else:
+        print("Higher number please")
+
+print(f"You have guessed the correct number ({n}) in {guesses} attempts.")
